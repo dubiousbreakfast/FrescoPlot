fbe468902c4fa04d393ad1d5485892582548b5b7
Caleb <camarsha@ncsu.edu>
2015-11-25 15:58:21 -0500

    Start of a general purpose reader for both angular states and energies

---
+++ FrescoReader.py
@@ -0,0 +1,29 @@
+#We look to take the output of the fort.3 and fort.16 files 
+#and put them in an easy to read file for plotting. The file 
+#called FresDat.dat
+
+import linecache
+
+
+class FresFil():
+
+    def __init__(self,list1,list2,output="FresDat.dat"):
+        self.spinfile = list1
+        self.diffcross = list2
+        self.output = output
+
+
+    def JTar(self):
+        
+            
+            
+                
+                    
+                    
+                    
+
+
+
+
+
+
