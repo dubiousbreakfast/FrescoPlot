4bc889cc1807f55ac70d8ce422f24f871d9f97e6
Caleb <camarsha@ncsu.edu>
2016-02-17 16:06:19 -0500

    Fucked up

--- FrescoExe.py
+++ FrescoExe.py
@@ -1,10 +1,11 @@
 import os
 
 
-def run():
+def filerun():
     filename = raw_input("Name of input file? ")
     output = str(filename)+'.out'
     command = 'fresco' + '<' + filename + '>' + output
     os.system(command)
     
 
+filerun()
